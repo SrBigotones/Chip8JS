02d91819df552c29a7b21cec8355950ec3eee3f0
AugustoMBA <augustoMBA@saitekGit.com>
2023-01-11 16:52:16 -0300

    first test of running something, not working yet

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const fs = require('fs')
+
 var vRAM = new Array(4096)
-var vREG = new Array(16) //V0 to VF general registers
-var vPC, vI, vSTACK, vTIMER, vSOUND
+var vVX = new Array(16) //V0 to VF general registers
+var vPC = vI = vTIMER = vSOUND = 0
+var oX = new Array(64).fill(0)
+var oY = new Array(32).fill(0)
+
+var vSTACK = new Array()
 
 const startRAM = 512 //Where I start to write to RAM
 const vFONTS = [
@@ -29,35 +35,128 @@ function writeFonts(start){
     }
 }
 
-function fetch(){}
+function fetch(){
+    let actualOP = 0
+    actualOP = (vRAM[startRAM+vPC] << 8) + (vRAM[startRAM+vPC+1]) //Big Endian
+    vPC += 2
+    return actualOP
+}
+
 function decode(opCODE){
+    let nibble = [(opCODE & 0x0F00) >> 8,(opCODE & 0x00F0) >> 4,(opCODE & 0x000F)]
     switch (opCODE) {
         case 0x00e0: // clear screen
+            ctx.clearRect(0,0, canvas.width, canvas.height)
+            oX = new Array(64).fill(0)
+            oY = new Array(32).fill(0)
             break;
+
         case (opCODE & 0xF000) == 0x1000: //1NNN Jump
+            vPC = opCODE & 0x0fff
             break;
+
+        case (opCODE & 0xF000) == 0x2000://2NNN Jump set stack
+            vSTACK.push(vPC)
+            vPC = opCODE & 0x0fff
+            break;
+
+        case opCODE == 0x00ee://00ee Return
+            vPC = vSTACK.pop()
+            break;
+
         case (opCODE & 0xF000) == 0x6000://6XNN set register VX
+            vVX[nibble[0]] = (nibble[1] << 4) + nibble[2]
             break;
+
         case (opCODE & 0xF000) == 0x7000://7XNN add value to register VX
+            vVX[nibble[0]] += (nibble[1] << 4) + nibble[2]
             break;
+
         case (opCODE & 0xF000) == 0xA000://ANNN set index register I
+            vI = opCODE & 0xFFF
             break;
+
         case (opCODE & 0xF000) == 0xD000://DXYN display/draw
+            let x = vVX[nibble[0]] 
+            let y = vVX[nibble[1]]
+            let pixelLine = 0
+            vVX[0xF] = 0 //VF
+            
+            for (let h = 0; h < nibble[2]; h++) {
+                pixelLine = vRAM[startRAM + vI + h]
+                for (let j = 0; j < 8; j++) {
+                    if((pixelLine & (1 << j)) != 0){
+                        if(oX[x+j] == 1 & oY[y+h] == 1){
+                            drawPixel(x+j, y+h, 0)
+                            vVX[0xf] = 1
+                        }else{
+                            drawPixel(x+j, y+h, 0)
+                        }
+                    }
+                    if(x+j > 64){
+                        break
+                    }
+                }
+                if(y+h > 32){
+                    break
+                }
+            }
+            
             break;
+
+
         default:
             break;
     }
 }
 
-function drawPixel(x, y){
+
+function loadPG(){
+    //Load program to RAM
+    let h = 0
+    fs.open('./programs/ibmLogo.ch8', 'r', function(err, fd) {
+    if (err)
+        throw err;
+    var buffer = Buffer.alloc(1);
+    while (true)
+    {   
+        var num = fs.readSync(fd, buffer, 0, 1, null);
+        if (num === 0)
+        break;
+        vRAM[startRAM + h]
+        h += 1
+        //console.log('byte read', buffer[0].toString(16));
+    }
+    });
+}
+function drawPixel(x, y, c){
     ctx.beginPath();
-    ctx.rect(x, y, 10, 10);
-    ctx.fillStyle = `white`;//`rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+    ctx.rect(x*10, y*10, 10, 10);
+    if(c == 1){
+        //draw
+        ctx.fillStyle = `white`;//`rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+        oX[x] = oY[y] = 1
+    }else{
+        //clean per se
+        ctx.fillStyle = `black`;//`rgb(${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)},${Math.floor(Math.random() * 255)})`;
+        oX[x] = oY[y] = 0
+    }
     ctx.fill();
     ctx.closePath();    
 }
 
+writeFonts(0x50)
 
-writeFonts(0x90)
+loadPG()
 
-console.log(vRAM[0])
+
+function startME(){
+    while (true) {
+        decode(fetch())    
+    }
+}
+
+
+
+
+
